0b26e8522da2f961643d57d3b971214498454209
Gaetan Lwamba <gaetanlwmb@gmail.com>
2019-10-30 16:08:39 +0200

    Adding config

---
+++ src/main/kotlin/org/safina/safinabackend/config/Config.kt
@@ -0,0 +1,18 @@
+package org.safina.safinabackend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+import org.safina.safinabackend.entity.*
+
+class RepositoryConfig : RepositoryRestConfigurerAdapter() {
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration) {
+        config.exposeIdsFor(Membre::class.java)
+        config.exposeIdsFor(Photo::class.java)
+
+        // config.exposeIdsFor(
+        //     entityManager.getMetamodel().getEntities().stream()
+        //     .map(Type::getJavaType)
+        //     .toArray(Class[]::new));
+    }
+}
