6bcc9b762088002acde2eb448ad2088b398848b5
Gaetan Lwamba <gaetanlwmb@gmail.com>
2019-10-31 13:36:02 +0200

    Adding new penses

--- src/main/kotlin/org/safina/safinabackend/SafinaBackendApplication.kt
+++ src/main/kotlin/org/safina/safinabackend/SafinaBackendApplication.kt
@@ -22,8 +22,8 @@ fun main(args: Array<String>) {
 	val membreDao = context.getBean(MembreDao::class.java)
 	val pouleDao = context.getBean(PouleDao::class.java)
 
-//	val pense = Pense(id = 1, nom = "Eric", contenue = "Content", date = "12/12/1999")
-//	penseDao.save(pense)
+	val pense = Pense(id = 1, nom = "Naissance de Don Bosco", contenue = "Naisa", date = "12/12/1999")
+	penseDao.save(pense)
 	val membre = Membre(1,"Gaetan","Gaetan","Safina","Safina","0974684530","1234",0.0,"gaetanlwmb")
 	membreDao.save(membre)
 	val poule = Poule(1,"pooule","12/12/2019",40,1,50.0)
